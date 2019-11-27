3f18f6cac8662f53408fce644fe7c67637daaf43
Tami Williams <tami@asktami.com>
2019-11-27 17:08:19 -0500

    no logging

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const logger = require('../logger');
 const userService = require('./users-service');
 
 const userRouter = express.Router();
