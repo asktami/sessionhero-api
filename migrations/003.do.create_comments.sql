4b31c2153e8ceb5f2c59dfca000be32d11b23092
Tami Williams <tami@asktami.com>
2019-11-30 02:02:29 -0500

    changed fieldname from text to comment

--- migrations/003.do.create_comments.sql
+++ migrations/003.do.create_comments.sql
@@ -1,10 +1,9 @@
 CREATE TABLE comments (
     id SERIAL PRIMARY KEY,
-    text TEXT NOT NULL,
+    comment TEXT NOT NULL,
     rating INTEGER NOT NULL,
     date_created TIMESTAMP DEFAULT now() NOT NULL,
     date_modified TIMESTAMP DEFAULT now() NOT NULL,
-    fullname TEXT,
     session_id VARCHAR
         REFERENCES sessions(id) ON DELETE CASCADE NOT NULL,
     user_id INTEGER
