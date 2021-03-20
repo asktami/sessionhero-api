6888d5d101f54006900cf6da6d8f2661f1343e48
Tami Williams <tami@asktami.com>
2021-03-20 17:48:24 -0400

    updated for cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,23 @@ app.use(
 	})
 );
 
-app.use(cors());
+//app.use(cors());
+
+// from CodeMentor
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Credentials: true');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	// res.header("Access-Control-Max-Age", "1000")
+	if (req.method == 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+	next();
+});
 
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
